774daa92d1e49549a4c064a4cceacd073afa5ffa
CD.Wang <jewelry_wolf@hotmail.com>
2012-11-04 22:26:06 -0800

    Only download not-existing files
    Also handled exceptions and keep downloading.

--- download_edgarfile.js
+++ download_edgarfile.js
@@ -25,28 +25,42 @@ function getSingleFile(remote_path) {
     getSingleFile(task_queue.pop());
     running_count--;
   } else {
-    console.log('downloading '+remote_path);
-    ftp.get(remote_path, function(err, data) {
-      if (err) console.error(err);
-      if (Buffer.isBuffer(data)) {
-        //console.log('content length:'+data.length);
-        fs.writeFile(path.resolve(local_cache_root,path.basename(remote_path)),data,'utf8',function(err){
-          if (err) console.error(err);
-          console.log(remote_path+' cached, content length: %d!', data.length);
-        });
-        if (task_queue.length>0) {
-          getSingleFile(task_queue.pop());
-        } else if (running_count==1) {
-          ftp.raw.quit(function(err, res) {
-            if (err) console.error(err);
-            console.log("FTP session finalized! See you soon!");
-          });
-        }
-      } else {
-        console.log(data);
+    if (fs.existsSync(path.resolve(local_cache_root,path.basename(remote_path)))) {
+      console.log('skipping '+remote_path);
+      if (task_queue.length>0) {
+        getSingleFile(task_queue.pop());
       }
       running_count--;
-    });
+    } else {
+      console.log('downloading '+remote_path);
+      ftp.get(remote_path, function(err, data) {
+        if (err) { 
+          console.error(err);
+          if (task_queue.length>0) {
+            getSingleFile(task_queue.pop());
+          }
+        } else {
+          if (Buffer.isBuffer(data)) {
+            //console.log('content length:'+data.length);
+            fs.writeFile(path.resolve(local_cache_root,path.basename(remote_path)),data,'utf8',function(err){
+              if (err) console.error(err);
+              console.log(remote_path+' cached, content length: %d!', data.length);
+            });
+            if (task_queue.length>0) {
+              getSingleFile(task_queue.pop());
+            } else if (running_count==1) {
+              ftp.raw.quit(function(err, res) {
+                if (err) console.error(err);
+                console.log("FTP session finalized! See you soon!");
+              });
+            }
+          } else {
+            console.log(data);
+          }
+        }
+        running_count--;
+      });
+    }
   }
 }
 
